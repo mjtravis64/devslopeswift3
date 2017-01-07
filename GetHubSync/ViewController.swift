85f6b798179f856ead8c267e1edbfc8c09278f30
Michael Travis <michaeltravis@MJT-iMac.local>
2017-01-07 13:54:57 -0600

    Initial Commit

---
+++ GetHubSync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetHubSync
+//
+//  Created by Michael Travis on 1/7/17.
+//  Copyright © 2017 Michael Travis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
